016eb2d0cef784f7084ee8afda397e8492b4c424
KV <lakrise347@gmail.com>
2023-05-01 17:08:26 +0300

    Автоматизация

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,31 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let header = document.querySelector('.page-header');
 
 navMain.classList.remove('main-nav--nojs');
+header.classList.remove('page-header--nojs');
 navMain.classList.add('main-nav--closed');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+  }
+  else
+    if (navMain.classList.contains('main-nav--opened') &! navMain.classList.contains('main-nav--closing')) {
+      navMain.classList.add('main-nav--closing');
+      timer = setTimeout(() => {
+        navMain.classList.remove('main-nav--closing');
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+      }
+      , 400);
+  }
+  else
+  if (navMain.classList.contains('main-nav--closing'))
+  {
+    clearTimeout(timer);
+    navMain.classList.remove('main-nav--closing');
+    navMain.classList.add('main-nav--opened');
   }
 });
